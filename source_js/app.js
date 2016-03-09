8024101155f132f933a2f75d6df23dd9b9f19f7e
Pret- <mwojci23@illinois.edu>
2016-03-08 20:23:27 -0600

    MP3 Submission

--- source_js/app.js
+++ source_js/app.js
@@ -1,5 +1,23 @@
-var app = angular.module('mp3',['ngRoute']);
-
-app.config(function ($routeProvider) {
-
-})
+var app=angular.module("mp3",[
+    "ngRoute",
+    "appController"
+]);
+app.config(['$routeProvider',
+    function($routeProvider){
+        $routeProvider.
+        when('/list', {
+            templateUrl: 'partials/list.html',
+            controller: 'ListCtrl'
+        }).
+        when('/list/:imdbID/:rank', {
+            templateUrl: 'partials/details.html',
+            controller: 'DetailCtrl'
+        }).
+        when('/gallery', {
+            templateUrl: 'partials/gallery.html',
+            controller: 'GalleryCtrl'
+        }).
+        otherwise({
+            redirectTo: '/list'
+        });
+    }]);
--- source_js/controllers.js
+++ source_js/controllers.js
@@ -1,1 +1,34 @@
 /* Sample Controller */
+var appController = angular.module('appController', []);
+
+appController.controller('ListCtrl', ['$scope', '$http',
+    function ($scope, $http) {
+        $http.get('./data/imdb250.json').success(function(data) {
+            $scope.movies = data;
+        });
+
+        $scope.sort = 'rank';
+    }]);
+
+appController.controller('DetailCtrl', ['$scope', '$routeParams', '$http',
+    function($scope, $routeParams, $http) {
+        $http.get('./data/imdb250.json').success(function(data) {
+            $scope.movies = data;
+        });
+
+        $scope.imdbID = $routeParams.imdbID;
+        $scope.rank = $routeParams.rank;
+        $scope.prev = $scope.rank - 1;
+        $scope.next = $scope.rank - (-1); // Subtract twice because if I use + it just truncates it and changes it to a string.
+    }]);
+
+appController.controller('GalleryCtrl', ['$scope', '$http',
+    function ($scope, $http) {
+        $http.get('./data/imdb250.json').success(function(data) {
+            $scope.movies = data;
+        });
+
+        $scope.genreFilter = '';
+    }]);
+
+
